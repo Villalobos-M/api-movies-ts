b7bd1ef3f42e6af2e54590ab8ae28fff8373fe32
Villalobos-M <mar.villalobosrv@gmail.com>
2022-12-09 10:26:54 -0600

    add atlas mongodb

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 
 async function dbConnect(): Promise<void> {
   const DB_URI = <string>process.env.DB_URI;
-  await connect(DB_URI);
+  await connect("mongodb+srv://M4rv:a1b2c3rt@movies-ts.kvaphd1.mongodb.net/?retryWrites=true&w=majority");
 }
 
 export default dbConnect;
